c378f8299a65a9ca14f86401f8e80f69fce9b7fb
jcoenep <jcoenep@hotmail.com>
2016-04-22 19:24:23 +0800

    tests init

---
+++ tests/testthat/test_all.R
@@ -0,0 +1,20 @@
+library(rodham)
+
+context("All tests")
+
+test_that("test all", {
+  # search
+  emails <- search_emails()
+  expect_equal(nrow(emails), 29444)
+  expect_equal(ncol(emails), 9)
+  expect_equal(names(emails), c("docID", "docDate", "to", "from",
+                                "originalTo", "originalFrom", "subject",
+                                "interesting", "not_interesting"))
+
+  # edges
+  expect_equal(nrow(edges_emails(emails)), 795)
+  expect_equal(ncol(edges_emails(emails)), 3)
+  expect_equal(ncol(edges_emails(emails, "subject")), 4)
+  expect_equal(names(edges_emails(emails, "subject", "docDate")),
+               c("from", "to", "subject", "docDate", "weight"))
+})
