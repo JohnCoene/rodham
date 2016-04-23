d2c32a86d6b68adc4197d44fff89b14910319e80
jcoenep <jcoenep@hotmail.com>
2016-04-23 21:19:45 +0800

    test nodes + error checks + test ext search FUN

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -10,11 +10,25 @@ test_that("test all", {
   expect_equal(names(emails), c("docID", "docDate", "to", "from",
                                 "originalTo", "originalFrom", "subject",
                                 "interesting", "not_interesting"))
+  # search
+  emails <- search_emails(internal = FALSE)
+  expect_equal(nrow(emails), 29444)
+  expect_equal(ncol(emails), 9)
+  expect_equal(names(emails), c("docID", "docDate", "to", "from",
+                                "originalTo", "originalFrom", "subject",
+                                "interesting", "not_interesting"))
 
   # edges
+  expect_error(edges_emails())
   expect_equal(nrow(edges_emails(emails)), 795)
   expect_equal(ncol(edges_emails(emails)), 3)
   expect_equal(ncol(edges_emails(emails, "subject")), 4)
   expect_equal(names(edges_emails(emails, "subject", "docDate")),
                c("from", "to", "subject", "docDate", "weight"))
+
+  # nodes
+  expect_error(nodes_emails())
+  expect_equal(nrow(nodes_emails(emails)), 421)
+  expect_equal(ncol(nodes_emails(emails)), 2)
+  expect_equal(names(nodes_emails(emails)), c("label", "weight"))
 })
