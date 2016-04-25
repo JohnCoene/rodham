c1e5ec8ccf4550b82c75de95897f31f0e3d222c4
jcoenep <jcoenep@hotmail.com>
2016-04-25 16:31:27 +0800

    raw2clean OStoURI checkRelease

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,35 @@ raw2clean <- function(x){
   return(clean)
 }
 
+checkRelease <- function(x){
+
+  x <- tolower(x)
+
+  if (x %in% db$name && length(x) == 1) {
+    release <- db[db$name == x, "uri"]
+    return(release)
+  } else if(!x %in% db$name && length(x) == 1) {
+    msg <- paste0(db$name, collapse = ", ")
+    stop(paste0("Wrong release, can be one of: ", msg), call. = FALSE)
+  } else if (length(x) > 1){
+    stop("Can only pass one release", call. = FALSE)
+  }
+}
+
+OStoURI <- function(x){
+  if(x == "Windows"){
+    uri <- "ftp://ftp.foolabs.com/pub/xpdf/xpdfbin-win-3.04.zip"
+    ext <- ".zip"
+  } else if (x == "Linux"){
+    uri <- "ftp://ftp.foolabs.com/pub/xpdf/xpdfbin-linux-3.04.tar.gz"
+    ext <- ".tar.gz"
+  } else if(x == "Darwin") {
+    uri <- "ftp://ftp.foolabs.com/pub/xpdf/xpdfbin-mac-3.04.tar.gz"
+    ext <- ".tar.gz"
+  } else {
+    stop(paste0("Unrecognised operating system, download manually from \n",
+                "http://www.foolabs.com/xpdf/download.html"), call. = FALSE)
+  }
+  lst <- list(uri = uri, ext = ext)
+  return(lst)
+}
