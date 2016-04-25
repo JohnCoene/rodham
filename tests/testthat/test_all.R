e8daea5c3577e76b2f9131a0802a4e1ceb31349e
jcoenep <jcoenep@hotmail.com>
2016-04-25 16:31:38 +0800

    cleanup

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -17,7 +17,6 @@ test_that("test all", {
   expect_equal(names(emails), c("docID", "docDate", "to", "from",
                                 "originalTo", "originalFrom", "subject",
                                 "interesting", "not_interesting"))
-
   # edges
   expect_error(edges_emails())
   expect_equal(nrow(edges_emails(emails)), 795)
@@ -25,10 +24,11 @@ test_that("test all", {
   expect_equal(ncol(edges_emails(emails, "subject")), 4)
   expect_equal(names(edges_emails(emails, "subject", "docDate")),
                c("from", "to", "subject", "docDate", "weight"))
-
   # nodes
   expect_error(nodes_emails())
   expect_equal(nrow(nodes_emails(emails)), 421)
   expect_equal(ncol(nodes_emails(emails)), 2)
   expect_equal(names(nodes_emails(emails)), c("label", "weight"))
+  # errors get
+  expect_error(get_emails())
 })
