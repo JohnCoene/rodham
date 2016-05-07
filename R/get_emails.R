7b89a3c99467bc788b2c7757bf76f8050225b5cb
jcoenep <jcoenep@hotmail.com>
2016-05-07 11:53:36 +0800

    doc fix + test save.dir + dir.create removed

--- R/get_emails.R
+++ R/get_emails.R
@@ -29,7 +29,7 @@
 #' The \code{extractor} argument is the full path to your pdftotext.exe
 #' extractor; visit \href{http://www.foolabs.com/xpdf/download.html}{xpdf} to
 #' download or try \code{\link{get_xpdf}} which attempts to download and
-#' extract the text to pdf exatractor.
+#' unzip the text to pdf extractor. See examples.
 #'
 #' @return Fetches email zip file from the WSJ and extract text files in
 #' \code{save.dir}, returns full path to directory that contains parsed txt
@@ -37,16 +37,23 @@
 #'
 #' @examples
 #' \dontrun{
+#' # get xpdf extractor
 #' ext <- get_xpdf()
+#'
+#' # create
+#' dir.create("./emails")
+#'
 #' # get emails released in august
-#' emails_cuba <- get_emails(release = "August", save.dir = "C:/",
+#' emails_aug <- get_emails(release = "August", save.dir = "./emails",
 #'                      extractor = ext)
 #'
 #' # use manually downloaded extractor
 #' ext <- "C:/xpdfbin-win-3.04/bin64/pdftotext.exe"
 #'
 #' # get emails related to Benghazi released in December
-#' emails_bengh <- get_emails(release = "Benghazi", extractor = ext)
+#' emails_bengh <- get_emails(release = "Benghazi", extractor = ext,
+#'                            save.dir = "./emails")
+#'
 #' files <- list.files(emails_bengh)
 #' content <- lapply(1:length(files), function(x){
 #'    readLines(paste0(emails_bengh, "/", files[[x]]))
@@ -65,13 +72,15 @@ get_emails <- function(release, save.dir = getwd(), extractor){
   if (!dir.exists(save.dir)) {
     stop("save.dir does not exist")
   }
-  if(!file.exists("C:/xpdfbin-win-3.04/bin64/pdftotext.exe")){
+  if(!file.exists(extractor)){
     stop("wrong path to extractor")
   }
-  # test extractor
-  v <- suppressWarnings(system(paste(extractor, "-v")))
+  v <- suppressWarnings(system(paste(extractor, "-v"))) # test extractor
   if (!v %in% c(99, 0, 1, 2, 3)) {
     stop("incorrect path to extractor, see get_xpdf")
+  }
+  if (!dir.exists(save.dir)) {
+    stop("save.dir does not exist")
   }
   uri <- checkRelease(release) # check release input and return URL
   temp_zip <- tempfile(fileext = ".zip") # create temp
@@ -80,21 +89,19 @@ get_emails <- function(release, save.dir = getwd(), extractor){
   unzip(zipfile = temp_zip, exdir = temp_dir) # unzip
   unlink("temp_zip", recursive = TRUE) # delete temp once unzipped
   files <- list.files(temp_dir)  # list files
-  files <- files[grep("pdf", files)] # only take pdf
+  files <- files[grep("pdf", files)] # only take pdf files
   dest <- gsub(".pdf", ".txt", files) # name destinations for extraction
-  save_dir <- file.path(save.dir, paste(release)) # build path to save
-  dir.create(save_dir) # create directory by release name
   cat("Extracting content from", length(files), "pdf files...\n")
   pb <- txtProgressBar(style = 3)
   for (i in 1:length(files)) {
     pdf <- paste0(temp_dir,"\\", files[i])
-    txt <- paste0(save_dir,"/", dest[i])
+    txt <- paste0(save.dir,"/", dest[i])
     system(paste(extractor, "-nopgbrk" , pdf, txt, sep = " "),
            wait = TRUE)
     setTxtProgressBar(pb, i/length(files))
   }
   unlink("temp_dir", recursive = TRUE) # delete temp once extracted
   close(pb)
-  message("emails .txt files saved at: ", save_dir)
-  return(save_dir)
+  message("emails .txt files saved at: ", save.dir)
+  return(save.dir)
 }
