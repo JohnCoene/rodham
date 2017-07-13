f6566d4e23d7de81f8ee1e5512657f35a9079099
John Coene <jcoenep@gmail.com>
2017-07-13 14:21:26 +0800

    extract addresses init

--- R/helpers.R
+++ R/helpers.R
@@ -40,7 +40,6 @@ load_emails <- function(dir){
   return(content)
 }
 
-
 #' Extract date
 #'
 #' Exctract dates from emails
@@ -79,12 +78,55 @@ load_emails <- function(dir){
 #'
 #' @export
 extract_date <- function(emails){
-  d <- stringr::str_extract_all(string = emails, pattern='\\w+\\s\\d+(st)?(nd)?(rd)?(th)?,\\s+\\d+')
+  d <- stringr::str_extract_all(string = emails, pattern = '\\w+\\s\\d+(st)?(nd)?(rd)?(th)?,\\s+\\d+')
   d <- lapply(d, function(x){
     as.Date(x, "%B %d, %Y")
   })
-  d <- lapply(d, unique)
   names(d) <- names(emails)
 
+  return(d)
+}
+
+#' Extract email addresses
+#'
+#' Exctract email addresses from emails
+#'
+#' @param emails list of email contents, as returned by \code{\link{load_emails}}
+#'
+#' @return named \code{list} of dates where names are emails/files name.
+#'
+#' @examples
+#' \dontrun{
+#' # get xpdf extractor
+#' ext <- get_xpdf()
+#'
+#' # create
+#' dir.create("emails")
+#'
+#' # get emails released in august
+#' emails_aug <- get_emails(release = "August", save.dir = "./emails",
+#'                      extractor = ext)
+#'
+#' # use manually downloaded extractor
+#' # ext <- "C:/xpdfbin-win-3.04/bin64/pdftotext.exe"
+#'
+#' # get emails related to Benghazi released in December
+#' emails_bengh <- get_emails(release = "Benghazi", extractor = ext,
+#'                            save.dir = "./emails")
+#'
+#' contents <- load_emails(emails_bengh)
+#'
+#' addresses <- extract_address(contents)
+#' }
+#'
+#' @seealso \code{\link{load_emails}}
+#'
+#' @author John Coene \email{jcoenep@gmail.com}
+#'
+#' @export
+extract_address <- function(emails){
+  pat <- '([_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4}))'
+  d <- stringr::str_extract_all(string = emails, pattern = pat)
+  names(d) <- names(emails)
   return(d)
 }
