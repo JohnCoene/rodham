babf54cc869622387c0792cb49caec694e460330
John Coene <jcoenep@gmail.com>
2017-07-13 14:53:57 +0800

    tests helpers

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -2,6 +2,8 @@ library(rodham)
 
 context("All tests")
 
+Sys.setlocale("LC_TIME", "C")
+
 test_that("test all", {
   # search
   emails <- search_emails()
@@ -19,4 +21,21 @@ test_that("test all", {
                c("from", "to", "subject", "docDate", "freq"))
   # errors get
   expect_error(get_emails())
+
+  #  1 email
+  contents <- structure(list(C05765907 = c("Sullivan, Jacob J <SullivanJJ@state.gov> Friday, December 4, 2009 3:31 AM",
+                                        "Iran", "The EU meets in the coming days, and we are hoping for a strong public - and private - position on Iran. Bill has identified 5 countries that need touching to help drive a good outcome:",
+                                        "I know Huma has discussed with you, but a 2-minute discussion with each that underscores the key points reflected on your card would do the trick, if you can swing it.",
+                                        "Tx. Also, the intervention, with your modifications, turned out well. The process, in this case, did not generate a good • enough product -- I tried to make it clearer and stronger this morning and your amendments helped a lot."
+  )), .Names = "C05765907")
+
+  dates <- extract_date(contents) # get dates
+  expect_equal(dates, list(
+    C05765907 = c(as.Date("2009-12-04"))
+  ))
+
+  address <- extract_address(contents)
+  expect_equal(address, list(
+    C05765907 = c("SullivanJJ@state.gov")
+  ))
 })
